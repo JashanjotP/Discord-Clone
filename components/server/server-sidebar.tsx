1baa42464cf0bf857a22dc2ae6a75a232f87990e
Jashanjot <jashanjotpanjrath@gmail.com>
2023-10-14 19:39:28 -0400

    Added functionality that renders the channels

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -6,6 +6,10 @@ import { ServerHeader } from "./server-header";
 import { ScrollArea } from "../ui/scroll-area";
 import { ServerSearch } from "./server-search";
 import { Hash, Mic, ShieldAlert, ShieldCheck, Video } from "lucide-react";
+import { Separator } from "../ui/separator";
+import { ServerSection } from "./server-section";
+import { ServerChannel } from "./server-channel";
+
 
 interface ServerSidebarProps {
     serverId: string;
@@ -109,7 +113,17 @@ export const ServerSidebar = async({
                         },
                     ]}/>
                 </div>
-              </ScrollArea>
+                <Separator className="bg-zinc-200 dark:bg-zinc-700 rounded-md my-2"/>
+             
+              {!!textChannels?.length && (
+                <div className="mb-2">
+                    <ServerSection sectionType="channels" channelType={ChannelType.TEXT} role ={role} label="Text Channels"/>
+                    {textChannels.map((channel) => (
+                        <ServerChannel key={channel.id} channel={channel} role={role} server={server}/>    
+                    ))}
+                </div>
+              )}
+            </ScrollArea>
         </div>
     );
 }
