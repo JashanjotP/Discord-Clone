d3c8bb1911292812aa470b3b9657490f96d7b739
Jashanjot <jashanjotpanjrath@gmail.com>
2023-10-16 13:28:56 -0400

    Added chat header

---
+++ components/providers/socket-provider.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { createContext,useContext,useEffect,useState } from "react";
+import {io as ClientIo} from "socket.io-client";
+
+type SocketContextType = {
+    socket: any | null;
+    isConnected: boolean;
+}
+
+const SocketContext = createContext<SocketContextType>({
+    socket: null,
+    isConnected: false
+});
+
+export const useSocket = () => {
+    return useContext(SocketContext);
+};
+
+export const SocketProvider = ({children }: {children: React.ReactNode}) => {
+    const [socket, setSocket] = useState(null); 
+    const [isConnected,setIsConnected] = useState(false);
+
+    useEffect(()=> {
+        const socketInsatnce = new (ClientIo as any)(process.env.NEXT_PUBLIC_SITE_URL!, {path: "/api/socket/io" , addTrailingSlash: false});
+
+        socketInsatnce.on("connect", () =>{
+            setIsConnected(true);
+        });
+
+        socketInsatnce.on("disconnect", () => {
+            setIsConnected(false);
+        })
+
+        setSocket(socketInsatnce);
+
+        return () => {
+            socketInsatnce.disconnect();
+        }
+    }, []);
+
+    return(
+        <SocketContext.Provider value={{socket,isConnected}}>
+            {children}
+        </SocketContext.Provider>
+    )
+}
